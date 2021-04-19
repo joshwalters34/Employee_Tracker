32455eadb9beb0f7b9d3394fd05bb6c35c1d8146
JWalters <joshwalters34@gmail.com>
2021-04-19 18:03:51 -0400

    updated conection file with dotenv to hide my db password in env file

--- JS/trackerDBConnection.js
+++ JS/trackerDBConnection.js
@@ -1,18 +1,27 @@
 const mysql = require('mysql');
+require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: 'localhost',
+const connection = mysql.createConnection(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+  }
+  // host: 'localhost',
 
-  // Your port, if not 3306
-  port: 3306,
+  // // Your port, if not 3306
+  // port: 3306,
 
-  // Your username
-  user: 'root',
+  // // Your username
+  // user: 'root',
 
-  // Be sure to update with your own MySQL password!
-  password: 'GoNiners02@',
-  database: 'ice_creamDB',
-});
+  // // Be sure to update with your own MySQL password!
+  // password: '',
+  // database: 'ice_creamDB',
+);
 
 connection.connect((err) => {
   if (err) throw err;
